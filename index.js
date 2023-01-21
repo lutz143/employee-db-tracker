222534b3a551ba1a413372f4f54e57373bc78b5d
Josh <lutz_143@yahoo.com>
2023-01-21 17:58:00 -0500

    added class for Roles to render the ID, title, dept, and salary

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // const fs = require('fs');
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
+const jsonToTable = require('json-to-table');
+const classIndex = require("./lib/template");
 
 // Connect to database
 const db = mysql.createConnection(
@@ -15,3 +17,77 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employees_db database.`)
 );
+
+// db.query('SELECT roles.role_id, roles.role_title, department.department_name, roles.role_salary FROM roles LEFT JOIN department ON roles.fk_department_id = department.department_id ORDER BY roles.role_id', function (err, results) {
+//   const tables = jsonToTable(results);
+//   console.log(tables);
+// });
+
+const sql = 'SELECT roles.role_id, roles.role_title, department.department_name, roles.role_salary FROM roles LEFT JOIN department ON roles.fk_department_id = department.department_id ORDER BY roles.role_id'
+const roleHeaders = 
+`ID     Title
+--     -----`
+const promptroleGate = () => {
+  return new Promise(function(resolve, reject){
+  db.query(
+      sql, 
+      function(err, rows){                                                
+          if(rows === undefined){
+              reject(new Error("Error rows is undefined"));
+          }else{
+              resolve(rows);
+          }
+      }
+  )})
+  .then(roleRender => {
+    console.log(roleRender)
+    let rolesTable = []
+
+    for(var key in roleRender) {
+      roleId = roleRender[key]["role_id"]
+      roleTitle = roleRender[key]["role_title"]
+      roleDept = roleRender[key]["department_name"]
+      roleSalary = roleRender[key]["role_salary"]
+
+      let role = new classIndex.Role(roleId, roleTitle, roleDept, roleSalary)
+      let name = role.getName()
+      let title = role.getTitle()
+      let dept = role.getDept()
+      let salary = role.getSalary()
+
+      rolesTable.push({ID:name, Title:title, Department:dept, Salary:salary})
+    }
+
+    console.table(rolesTable)
+    process.exit(0);
+  })
+}
+
+
+const init = () => {
+  promptroleGate()  
+}
+
+init();
+
+getEmployeeNames = function(){
+  return new Promise(function(resolve, reject){
+    db.query(
+        sql, 
+        function(err, rows){                                                
+            if(rows === undefined){
+                reject(new Error("Error rows is undefined"));
+            }else{
+                resolve(rows);
+            }
+        }
+    )}
+)}
+
+// getEmployeeNames()
+//   .then(function(results){
+//     console.log(results);
+// })
+//   .catch(function(err){
+//     console.log("Promise rejection error: "+err);
+// })
