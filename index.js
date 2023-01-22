39567126fb124f8456d49f8a8bce8db36e1907e8
Josh <lutz_143@yahoo.com>
2023-01-22 10:43:46 -0500

    updates to template.js and index.js to render Employee information via node

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const db = mysql.createConnection(
 
 const sqlDept = 'SELECT department_id, department_name FROM department ORDER BY department_name'
 const sqlRole = 'SELECT roles.role_id, roles.role_title, department.department_name, roles.role_salary FROM roles LEFT JOIN department ON roles.fk_department_id = department.department_id ORDER BY roles.role_id'
+const sqlEmployee = 'SELECT employee_1.employee_id, employee_1.first_name, employee_1.last_name, roles.role_title, department.department_name, roles.role_salary, concat(employee.first_name, " ",employee.last_name) AS manager_name FROM department LEFT JOIN (roles LEFT JOIN (employee RIGHT JOIN employee AS employee_1 ON employee.employee_id = employee_1.manager_id) ON roles.role_id = employee_1.fk_role_id) ON department.department_id = roles.fk_department_id WHERE (((employee_1.employee_id) Is Not Null)) ORDER BY employee_1.employee_id'
 
 const promptDeptGate = () => {
   return new Promise(function(resolve, reject){
@@ -91,8 +92,61 @@ const promptRoleGate = () => {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+const promptEmployeeGate = () => {
+  return new Promise(function(resolve, reject){
+  db.query(
+      sqlEmployee, 
+      function(err, rows){                                                
+          if(rows === undefined){
+              reject(new Error("Error rows is undefined"));
+          }else{
+              resolve(rows);
+          }
+      }
+  )})
+  .then(employeeRender => {
+    // console.log(employeeRender)
+    let rolesTable = []
+
+    for(var key in employeeRender) {
+      let employeeId = employeeRender[key]["employee_id"]
+      let firstName = employeeRender[key]["first_name"]
+      let lastName = employeeRender[key]["last_name"]
+      let roleTitle = employeeRender[key]["role_title"]
+      let deptName = employeeRender[key]["department_name"]
+      let roleSalary = parseInt(employeeRender[key]["role_salary"])
+      let managerName = employeeRender[key]["manager_name"]
+
+      let employee = new classIndex.Employee(employeeId, firstName, lastName, roleTitle, deptName, roleSalary, managerName)
+      let employee_id = employee.getEmployeeId()
+      let first_name = employee.getEmployeeFirstName()
+      let last_name = employee.getEmployeeLastName()
+      let employee_title = employee.getEmployeeTitle()
+      let employee_dept = employee.getEmployeeDeptName()
+      let employee_salary = employee.getEmployeeSalary()
+      let employee_manager = employee.getEmployeeManagerName()
+
+      rolesTable.push({ID:employee_id, "First Name":first_name, "Last Name":last_name, Title:employee_title, Department:employee_dept, Salary:employee_salary, Manager:employee_manager})
+    }
+
+    console.table(rolesTable)
+    process.exit(0);
+  })
+}
+
+
 const init = () => {
-  
+  promptEmployeeGate()
   // promptDeptGate()
   // promptRoleGate()  
 }
